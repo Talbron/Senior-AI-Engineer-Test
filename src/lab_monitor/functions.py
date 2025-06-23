bf57622ccbd9f5a4043f0121608a37eb2d7a635b
Matt Freeland <matthew_freeland@yahoo.co.uk>
2025-06-23 12:17:48 +0100

    added boilerplate and workflows

---
+++ src/lab_monitor/functions.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+"""
+    test_dummy.py:
+    quick dummy test to check pipeline.
+"""
+
+class DummyClass:
+    """
+        A dummy class to test coverarge
+    """
+    def __init__(self):
+        self.val = 1
---
+++ tests/test_dummy.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+"""
+    test_dummy.py:
+    quick dummy test to check pipeline.
+"""
+from lab_monitor.functions import DummyClass
+
+def test_example():
+    dummy = DummyClass()
+    assert dummy.val == 1
